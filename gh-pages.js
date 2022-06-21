e0f04a968ae765d2ffca2a96ea66fe36af3cbb02
anirudhsnayak <anirudhsnayak@gmail.com>
2022-06-20 20:34:02 -0700

    Incorporated Google Analytics

--- gh-pages.js
+++ gh-pages.js
@@ -4,10 +4,10 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/username/yourproject.git', // Update to point to your repository  
+        repo: 'https://github.com/anirudhsnayak/webpage.git', // Update to point to your repository  
         user: {
-            name: 'Your name', // update to use your name
-            email: 'Your Email address' // Update to use your email
+            name: 'Anirudh Nayak', // update to use your name
+            email: 'anirudhsnayak@gmail.com' // Update to use your email
         }
     },
     () => {
