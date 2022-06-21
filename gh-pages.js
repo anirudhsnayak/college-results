842081704753804b86efbe63ea31334fb0819539
anirudhsnayak <anirudhsnayak@gmail.com>
2022-06-20 20:47:46 -0700

    added images

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/anirudhsnayak/webpage.git', // Update to point to your repository  
+        repo: 'https://github.com/anirudhsnayak/college-results.git', // Update to point to your repository  
         user: {
             name: 'Anirudh Nayak', // update to use your name
             email: 'anirudhsnayak@gmail.com' // Update to use your email
