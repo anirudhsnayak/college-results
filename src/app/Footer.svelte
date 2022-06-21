4772e3ad3c7c82719f80aab7560bf151869cb5eb
anirudhsnayak <anirudhsnayak@gmail.com>
2022-06-21 15:15:28 -0700

    Added footer

---
+++ src/app/Footer.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<div class="footer">
+    <div class="privacy-policy">
+        <a href="https://anirudhsnayak.github.io/privacy-policy/" target="_blank">Privacy Policy</a>
+    </div>
+</div>
+
+<style>
+.footer{
+    width: 100%;
+    min-height: 5rem;
+    flex: 1 1 auto;
+    display: grid;
+    top: 0;
+    bottom: 0;
+}
+.privacy-policy{
+    align-self: end;
+    justify-self: center;
+    margin-right: 1rem;
+    margin-bottom: 1rem;
+}
+</style>
--- src/app/Main.svelte
+++ src/app/Main.svelte
@@ -3,20 +3,24 @@
     import DatabaseQueryResults from "./DatabaseQuery/DatabaseQueryResults.svelte";
     import DatabaseSearch from "./DatabaseSearch.svelte";
     import DatabaseTitle from "./DatabaseTitle.svelte";
-import FaqModal from "./FAQ/FAQModal.svelte";
+    import FaqModal from "./FAQ/FAQModal.svelte";
+    import Footer from "./Footer.svelte";
     Database.importDatabase("data/collegeresults_simplify.json");
 </script>
 
 <div class="body">
-    <FaqModal />
+    <FaqModal/>
     <DatabaseTitle/>
     <DatabaseSearch/>
     <DatabaseQueryResults/>
+    <Footer/>
 </div>
 
 <style type="text/scss">
 //background color black
     .body {
+        display: flex;
+        flex-direction: column;
         background-color: #000000;
         min-height: 100%;
         width: 100%;
